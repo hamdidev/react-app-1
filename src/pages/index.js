a3344716c1c9c1890534784439aeadaaa8f246e6
hamdidev <itechtranss77@gmail.com>
2020-01-29 22:27:24 +0300

    Header and Hero Section are done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Header from '../components/header'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to design and code React Apps</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, iusto?
-    </p>
-    <Link to="/page-2/">Watch the video</Link>
+    <Header />
+    <div className="hero">
+      <div className="heroGroup">
+        <h1>Learn to design and code React Apps</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </div>
 )
 
